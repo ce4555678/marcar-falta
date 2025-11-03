e49433bb62be3cb095aebf3c02097dfb395c2e75
Carlos Eduardo <ce4555678@gmail.com>
2025-11-03 17:07:51 -0300

    suspense no page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import SelectMonth from "@/components/selectMonth";
 import TableMonth from "@/components/tableMonth";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="mx-auto py-4 px-4 flex w-full items-center justify-center min-h-screen bg-linear-to-br from-slate-50 to-slate-100">
       <div className="w-full max-w-2xl grid grid-cols-1 gap-5">
-        <SelectMonth />
-        <TableMonth />
+        <Suspense>
+          <SelectMonth />
+          <TableMonth />
+        </Suspense>
       </div>
     </div>
   );
