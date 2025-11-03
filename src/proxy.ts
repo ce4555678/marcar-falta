0da5150b94b144c13187df1697388b2f7cc3539a
Carlos Eduardo <ce4555678@gmail.com>
2025-11-03 17:11:56 -0300

    resolvendo middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,12 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function proxy(request: NextRequest) {
+    const url = new URL(request.url);
+    const pathname = url.pathname;
     const sessionCookie = getSessionCookie(request);
 
     // THIS IS NOT SECURE!
     // This is the recommended approach to optimistically redirect users
     // We recommend handling auth checks in each page/route
     if (!sessionCookie) {
+        if (pathname == "/login") {
+            return NextResponse.next();
+        }
+
         return NextResponse.redirect(new URL("/login", request.url));
     }
 
@@ -15,5 +21,8 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/dashboard"], // Specify the routes the middleware applies to
-};
+    matcher: [
+        // Exclude API routes, static files, image optimizations, and .png files
+        '/((?!api|_next/static|_next/image|.*\\.png$).*)',
+    ],
+}
